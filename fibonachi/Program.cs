8183489b832a5830baf9f074d0feb6a88b0d943f
LOQ <LOQ@DESKTOP-FUMHBU1>
2023-11-01 11:26:08 +0330

    Add project files.

---
+++ fibonachi/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+                int a = 0, b = 1, c;
+                Console.WriteLine(" enter your number");
+                int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{a}\t{b}\t");
+for (int i = 2; i <= n; i++) 
+{
+    c = a + b;
+    Console.WriteLine($"{c}\t");
+    a = b;
+    b = c;
+}
+  // amirhossein azarakhsh.tamrine4
